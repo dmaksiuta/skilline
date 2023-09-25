4a48d8e115dcb04ce66c0bfd6a30fd740d7dc3c3
Isaeveg <isaev.egor.08@gmail.com>
2023-09-25 20:56:34 +0300

    init repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Welcome } from "./components/Welcome";
 import { Soft } from "./components/Soft";
-import { With } from "./components/With";
+import { Classroom } from "./components/Classroom";
 import { Features } from "./components/Features";
 import { News } from "./components/News";
 
@@ -9,7 +9,7 @@ function App() {
     <>
       <Welcome />
       <Soft />
-      <With />
+      <Classroom />
       <Features />
       <News />
     </>
---
+++ src/components/Classroom.jsx
@@ -0,0 +1,40 @@
+import withBg from "../assets/withbg.png";
+
+export const With = () => {
+  return (
+    <>
+      <div className="classroom">
+        <div className="classroom__container">
+          <div className="classroom__info">
+            <h2 className="classroom__title">
+              Everything you can do in a physical classroom,
+              <span className="classroom__span">you can do with Skilline</span>
+            </h2>
+            <p className="classroom__desc">
+              Skilline’s school management software helps traditional and online
+              schools manage scheduling, attendance, payments and virtual
+              classrooms all in one secure cloud-based system.
+            </p>
+            <a href="/" className="classroom__a">
+              Learn more
+            </a>
+          </div>
+          <div className="classroom__avatar">
+            <div className="classroom__border">
+              <div className="with__border-bg"></div>
+              <a href="/">
+                <img
+                  width="705px"
+                  height="471.079px"
+                  className="classroom__img"
+                  src={withBg}
+                  alt="Play"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
